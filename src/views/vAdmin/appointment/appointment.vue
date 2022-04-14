f43ce17da5192469a1e5b01540d9b23fd5ed3fda
儿时 <58261676+childrentime@users.noreply.github.com>
2022-04-14 22:13:09 +0800

    add appointment

--- src/views/vAdmin/appointment/appointment.vue
+++ src/views/vAdmin/appointment/appointment.vue
@@ -23,7 +23,70 @@
       </el-form>
     </div> -->
     <div>
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary" @click="dialogFormVisible = true"
+        >新增</el-button
+      >
+      <el-dialog title="发布预约名额" :visible.sync="dialogFormVisible">
+        <el-form :model="form" ref="form" :rules="rules">
+          <el-form-item
+            label="疫苗名称"
+            :label-width="formLabelWidth"
+            prop="name"
+          >
+            <el-input v-model="form.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item
+            label="类别"
+            :label-width="formLabelWidth"
+            prop="frequency"
+          >
+            <el-select v-model="form.frequency" placeholder="请选择类别">
+              <el-option label="第一针" value="第一针"></el-option>
+              <el-option label="第二针" value="第二针"></el-option>
+              <el-option label="第三针" value="第三针"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item
+            label="开始时间"
+            :label-width="formLabelWidth"
+            prop="startTime"
+          >
+            <el-date-picker
+              v-model="form.startTime"
+              type="datetime"
+              value-format="yyyy-MM-dd hh:mm:ss"
+              placeholder="选择开始时间"
+            >
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item
+            label="结束时间"
+            :label-width="formLabelWidth"
+            prop="endTime"
+          >
+            <el-date-picker
+              v-model="form.endTime"
+              value-format="yyyy-MM-dd hh:mm:ss"
+              type="datetime"
+              placeholder="选择结束时间"
+            >
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item
+            label="容量"
+            :label-width="formLabelWidth"
+            prop="capacity"
+          >
+            <el-input v-model="form.capacity" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="resetForm('form')">取 消</el-button>
+          <el-button type="primary" @click="submitForm('form')"
+            >确 定</el-button
+          >
+        </div>
+      </el-dialog>
     </div>
     <div>
       <el-row>
@@ -51,25 +114,77 @@ export default {
   name: "Appointment",
   data() {
     return {
-      formInline: {
-        user: "",
-        region: "",
-        phone: "",
-        card: "",
-      },
+      dialogFormVisible: false,
       tableData: [],
+      form: {
+        name: "",
+        frequency: "",
+        startTime: "",
+        endTime: "",
+        capacity: "",
+      },
+      rules: {
+        name: [{ required: true, message: "请输入名称", trigger: "blur" }],
+        frequency: [
+          { required: true, message: "请选择类别", trigger: "change" },
+        ],
+        startTime: [
+          { required: true, message: "请选择开始时间", trigger: "blur" },
+        ],
+        endTime: [
+          { required: true, message: "请选择结束时间", trigger: "blur" },
+        ],
+        capacity: [{ required: true, message: "请输入容量", trigger: "blur" }],
+      },
+      formLabelWidth: "120px",
     };
   },
   created() {
     const adminId = sessionStorage.getItem("vadminId");
-    this.$axios.get(`${prefix}/getVacc?adminId=${adminId}`).then((data) => {
-      const { data: tableData } = data.data;
-      this.tableData = tableData;
-    });
+    this.getData(adminId);
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    getData: function (adminId) {
+      this.$axios.get(`${prefix}/getVacc?adminId=${adminId}`).then((data) => {
+        const { data: tableData } = data.data;
+        this.tableData = tableData;
+      });
+    },
+    submitForm(formName) {
+      const adminId = sessionStorage.getItem("vadminId");
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          const { name, frequency, startTime, endTime, capacity } = this.form;
+          this.$axios
+            .post(
+              `${prefix}/publish?name=${name}&frequency=${frequency}&startTime=${startTime}&endTime=${endTime}&capacity=${capacity}&adminId=${adminId}`
+            )
+            .then((data) => {
+              const { code } = data.data;
+              if (code === 200) {
+                this.$message({
+                  showClose: true,
+                  message: "添加成功",
+                  type: "success",
+                });
+                this.getData(adminId);
+                this.resetForm(formName);
+              } else {
+                this.$message({
+                  showClose: true,
+                  message: "添加失败，请联系管理员",
+                  type: "error",
+                });
+              }
+            });
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+      this.dialogFormVisible = false;
     },
   },
 };
